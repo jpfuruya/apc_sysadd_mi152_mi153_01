a6a28ba57da020f9da4a9ba1502fcd7b7d9b7223
John Rafael Furuya <jpfuruya@gmail.com>
2017-08-09 18:00:11 +0800

    created the model for the prototype. models/In.php

--- Hotel Service Request and Report System/basic/views/layouts/main.php
+++ Hotel Service Request and Report System/basic/views/layouts/main.php
@@ -38,9 +38,7 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Ticket', 'url' => ['/site/ticket']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
