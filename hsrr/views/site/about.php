2f8fc724ca7d3400db85ce70c7f77770ad5dc4d9
chavezorly <chavez.orly23@gmail.com>
2017-09-08 20:08:40 +0800

    added views

---
+++ hsrr/views/site/about.php
@@ -0,0 +1,28 @@
+
+<br>
+<br>
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+
+$this->title = 'About';
+
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+    <h3>The View of the Ticket</h3>
+        <img src = "tixview.png" width="1000" height="400"/> <br>
+    <h3>Creating the Ticket</h3>
+        <img src = "create.png" width="1000" height="800"/> <br> <br>
+    <h3>Submitting Ticket</h3>
+        <img src = "submit.png" width="1000" height="800"/><br>
+    
+    </p>
+
+   
+</div>
